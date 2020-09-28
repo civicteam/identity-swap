610fc7168f6a3c764ff9933676602c48a32b4c2f
dankelleher <dankelleher@yahoo.com>
2020-09-28 10:22:07 +0200

    HE-12 Improve reliablility fo the solana web3 SDK by adding a retry function.

--- src/api/connection/index.ts
+++ src/api/connection/index.ts
@@ -2,6 +2,7 @@ import { clusterApiUrl, Commitment, Connection } from "@solana/web3.js";
 import { identity, memoizeWith } from "ramda";
 import { ExtendedCluster } from "../../utils/types";
 import { defaultCommitment } from "../../utils/env";
+import { retryableProxy } from "../../utils/retryableProxy";
 
 const LOCALNET_URL = "http://localhost:8899";
 
@@ -11,7 +12,14 @@ export const DEFAULT_COMMITMENT: Commitment = defaultCommitment;
 // Since connection objects include state, we memoise them here per network
 const createConnection = memoizeWith<(network: string) => Connection>(
   identity,
-  (network) => new Connection(network, DEFAULT_COMMITMENT)
+  (network) => {
+    const connection = new Connection(network, DEFAULT_COMMITMENT);
+
+    // Due to an issue with the solana back-end relating to CORS headers on 429 responses
+    // Rate-limiting responses are not retried correctly. Adding this proxy fixes this.
+    connection.getAccountInfo = retryableProxy(connection.getAccountInfo);
+    return connection;
+  }
 );
 
 export const getNetwork = (cluster: ExtendedCluster): string =>
---
+++ src/utils/retryableProxy.test.ts
@@ -0,0 +1,67 @@
+import { retryableProxy } from "./retryableProxy";
+
+type AsyncFunction = () => Promise<boolean>;
+const passAfter = (attempts: number): AsyncFunction => {
+  let attemptsRemaining = attempts;
+
+  return () => {
+    if (attemptsRemaining <= 1) return Promise.resolve(true);
+
+    attemptsRemaining--;
+
+    return Promise.reject(new Error("Test Reject"));
+  };
+};
+
+interface CountingFunction extends AsyncFunction {
+  counter: number;
+}
+
+const count = (fn: AsyncFunction) => {
+  const result: CountingFunction = function () {
+    result.counter++;
+    return fn();
+  };
+  result.counter = 0;
+  return result;
+};
+
+describe("retryableProxy", () => {
+  let reliableFunction: CountingFunction;
+  let unreliableFunction: CountingFunction;
+  let veryUnreliableFunction: CountingFunction;
+
+  beforeEach(() => {
+    reliableFunction = count(passAfter(1));
+    unreliableFunction = count(passAfter(3));
+    veryUnreliableFunction = count(passAfter(10));
+  });
+
+  it("should return immediately if the promise is resolved", async () => {
+    const passFirstTime = retryableProxy(reliableFunction);
+
+    await expect(passFirstTime()).resolves.toEqual(true);
+
+    expect(reliableFunction.counter).toEqual(1);
+  });
+
+  it("should retry until the promise is resolved", async () => {
+    const retryThreeTimes = retryableProxy(unreliableFunction, {
+      intervalMS: 5,
+    });
+
+    await expect(retryThreeTimes()).resolves.toEqual(true);
+
+    expect(unreliableFunction.counter).toEqual(3);
+  });
+
+  it("should fail after retrying max times", async () => {
+    const retryFiveTimes = retryableProxy(veryUnreliableFunction, {
+      intervalMS: 5,
+    });
+
+    await expect(retryFiveTimes()).rejects.toEqual(new Error("Test Reject"));
+
+    expect(veryUnreliableFunction.counter).toEqual(5);
+  });
+});
