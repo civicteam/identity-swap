4eccae1b36b119263f56c6ddaee58930574dbd0c
dankelleher <dankelleher@yahoo.com>
2020-11-11 08:48:35 +0100

    Some test and CI fixes

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -16,12 +16,8 @@ import { HasEqual } from "./utils/types";
 
 const superstruct = jest.requireActual("superstruct/umd/superstruct");
 jest.setMock("superstruct/lib/index.cjs", superstruct);
-jest.setMock(
-  "@solana/spl-token-swap/node_modules/superstruct/lib/index.cjs",
-  superstruct
-);
 // jest.setMock(
-//   "@civic/spl-identity/node_modules/superstruct/lib/index.cjs",
+//   "@solana/spl-token-swap/node_modules/superstruct/lib/index.cjs",
 //   superstruct
 // );
 jest.setMock("superstruct", superstruct);
